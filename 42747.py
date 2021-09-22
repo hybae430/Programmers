a2b6d10f59f609a8a9a12c2f3d33d11700e8da17
hayoung <violet9643@naver.com>
2021-09-22 11:38:27 +0900

    210922

--- 42747.py
+++ 42747.py
@@ -1,2 +1,16 @@
 # H-Index
+# 2021-09-21
 
+def solution(citations):
+    answer = 0
+    citations.sort(reverse=True)
+
+    for idx, c in enumerate(citations):
+        if idx + 1 <= c:
+            answer += 1
+        else:
+            break
+
+    return answer
+
+print(solution([9, 9, 9, 9, 12]))
--- 42839.py
+++ 42839.py
@@ -1,2 +1,39 @@
 # 소수 찾기
+# 2021-09-22
 
+from itertools import permutations
+
+# 여기는 에라토스테네스가 시간초과가 뜬다...;
+# def era(n):
+#     table = [False, False] + [True] * (n - 1)
+#
+#     for i in range(2, int(n ** 0.5) + 1):
+#         if table[i]:
+#             for j in range(i * 2, n + 1, i):
+#                 table[j] = False
+#
+#     return [i for i in range(2, n + 1) if table[i]]
+
+def solution(numbers):
+    answer = []
+
+    for i in range(1, len(numbers) + 1):
+        tmp = list(permutations(numbers, i))
+        for j in range(len(tmp)):
+            target = int(''.join(map(str, tmp[j])))
+            valid = True
+
+            if target < 2:
+                continue
+            else:
+                for k in range(2, int(target ** 0.5) + 1):
+                    if target % k == 0:
+                        valid = False
+                        break
+
+            if valid:
+                answer.append(target)
+
+    return len(set(answer))
+
+print(solution("17"))
